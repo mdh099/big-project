79a5dab7405dea77e1e54c8ead0e5e4998a283b3
MrRadir <83057195+MrRadir@users.noreply.github.com>
2022-04-05 19:41:28 -0400

    got bolts shooting

---
+++ AR-Asteroids/Assets/Scripts/ProjectileGunTutorial.cs
@@ -0,0 +1,97 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+/// Thanks for downloading my projectile gun script! :D
+/// Feel free to use it in any project you like!
+/// 
+/// The code is fully commented but if you still have any questions
+/// don't hesitate to write a yt comment
+/// or use the #coding-problems channel of my discord server
+/// 
+/// Dave
+public class ProjectileGunTutorial : MonoBehaviour
+{
+    //bullet 
+    public GameObject bullet;
+
+    //Gun stats
+    public float timeBetweenShooting, timeBetweenShots;
+    public int bulletsPerTap;
+    public bool allowButtonHold;
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //Reference
+    public Camera fpsCam;
+    public Transform attackPoint;
+
+    //bug fixing :D
+    public bool allowInvoke = true;
+
+    private void Awake()
+    {
+        //make sure magazine is full
+        //bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+
+    private void Update()
+    {
+        MyInput();
+    }
+    private void MyInput()
+    {
+        //Check if allowed to hold down button and take corresponding input
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        //Shooting
+        if (readyToShoot && shooting)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+
+        //Find the exact hit position using a raycast
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of your current view
+        RaycastHit hit;
+
+        //check if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+            targetPoint = hit.point;
+        else
+            targetPoint = ray.GetPoint(75); //Just a point far away from the player
+
+        //Calculate direction from attackPoint to targetPoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //Calculate new direction with spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(0, 0, 0);
+
+        //Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
+        //Rotate bullet to shoot direction
+        currentBullet.transform.TransformDirection(directionWithoutSpread.forward);
+
+        //Invoke resetShot function (if not already invoked), with your timeBetweenShooting
+        if (allowInvoke)
+        {
+            Invoke("ResetShot", timeBetweenShooting);
+            allowInvoke = false;
+        }
+
+    }
+    private void ResetShot()
+    {
+        //Allow shooting and invoking again
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+}
