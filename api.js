3a43b72e5304161d74a4c811298d17fb14abcda7
Logan Blair <loganb3142@gmail.com>
2022-04-07 21:34:26 -0400

    Optimized friends and score APIs and removed Cards APIs

--- api.js
+++ api.js
@@ -121,186 +121,193 @@ exports.setApp = function(app, client){
     });
 
 // Add Friend
+// Incoming: userID(current user), friendID(friend to add)  
+// Outgoing: error message
 app.post('/api/addfriend', async function(req, res, next)  
 {
-  // incoming: userID(current user), friendID(friend to add)
-  // outgoing: friendID(added friend), error message
+  // Gets input JSON
   const { userID, friendID } = req.body;
 
+  // Initializes error
   var error = '';
-
+  
+  // Attempts to find the current user and the friend to be added
   const currentUser = await User.findOne({ userID:userID });
   const friendToAdd = await User.findOne({ userID:friendID });
 
-  currentUser.Friends.push(friendID);
-  currentUser.save();
-
-  var ret = { friendID:friendID, error:''};
-  res.status(200).json(ret);
-});
-
-// Delete Friend
-app.post('/api/deletefriend', async function(req, res, next)
-{
-  // incoming: userID(current user logged in), friendID(friend to delete)
-  // outgoing: friendID(deleted friend), error message
-  
-  const { userID, friendID } = req.body;
-
-  var error = '';
-
-  await User.findOneAndUpdate({userID:userID}, {$pull: {Friends:friendID}});
-
-  var ret = { friendID:friendID, error:''};
-  res.status(200).json(ret);
-});
-
-// Search for friends to add
-app.post('/api/searchnewfriends', async function(req, res, next)
-{
-  // incoming: userID(current user logged in)
-  // outgoing: users(list of all possible accounts for user to befriend), error message
-
-  const { userID } = req.body;
-
-  var error = '';
-
-  const currentUser = await User.findOne( {userID: userID} );
-
-  const friendsPlusUser = currentUser.Friends;
-  friendsPlusUser.push(userID);
-
-  const users = await User.find({ userID: { $nin: friendsPlusUser} });
-
-  var ret = {users, error:''};
-  res.status(200).json(ret);
-});
-
-// Search current friends
-app.post('/api/searchcurrentfriends', async function(req, res, next)
-{
-  // incoming: userID(current user logged in)
-  // outgoing: currentFriends(user's friends), error message
-
-  const { userID } = req.body;
-
-  var error = '';
-
-  const currentUser = await User.findOne( {userID: userID} );
-
-  const currentFriends = await User.find( {userID: {$in: currentUser.Friends} } );
-
-  var ret = {currentFriends, error:''};
-  res.status(200).json(ret);
-});
-
-
-// Add new score
-app.post('/api/addscore', async function(req, res, next)
-{
-  // incoming: username, score
-  // outgoing: -
-
-  const { userID, username, score } = req.body;
-
-  var error = '';
-
-  const newScore = {userID, Username:username, Score:score};
-
-  var error = '';
-
-  try
-  {
-    const result = await Score.create(newScore);
-  }
-  catch(e)
-  {
-    error = e.toString();
+  // If-else statement to catch errors
+  if(currentUser){ // Error if current user doesn't exist
+    if(friendToAdd){ // Error if friend to add doesn't exist
+      if(currentUser.Friends.includes(friendID)){ // Error if user is already friends with friend to be added
+        error = 'Error. Already friends.';
+      }else{
+        currentUser.Friends.push(friendID);
+        currentUser.save();  
+      }
+    }else{
+      error = 'Error. Friend does not exist.';
+    }
+  }else{
+    error = 'Error. Current user does not exist.';
   }
 
-  var ret = {error:''};
+  // Return with error message
+  var ret = { error };
   res.status(200).json(ret);
 });
 
-// Show local leaderboard
-app.post('/api/showlocalleaderboard', async function(req, res, next)
-{
-  // incoming: userID(current user logged in)
-  // outgoing: username and scores of the current user every user that the current user is friends with
-  
-  const { userID } = req.body;
-
-  var error = '';
-
-  const currentUser = await User.findOne( {userID: userID} );
-
-  const friendsPlusUser = currentUser.Friends;
-  friendsPlusUser.push(userID);
-
-  const localScores = await Score.find( {userID: {$in: friendsPlusUser}} ).sort( {Score: -1 } );
-
-  var ret = {localScores, error:''};
-  res.status(200).json(ret);
-});
-
-// Show global leaderboard
-app.post('/api/showgloballeaderboard', async function(req, res, next)
-{
-  // incoming: -
-  // outgoing: username and scores of every user
-
-  var error = '';
-
-  const globalScores = await Score.find().sort( {Score: -1 } );
-
-  var ret = {globalScores, error:''};
-  res.status(200).json(ret);
-});
-
-/*
-
-// Olds cards apis
-
-app.post('/api/searchcards', async (req, res, next) => 
-{
-  // incoming: userId, search
-  // outgoing: results[], error
-  var error = '';
-  const { userId, search } = req.body;
-  var _search = search.trim();
-  
-  const db = client.db();
-  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
-  
-  
-
-  var _ret = [];
-  for( var i=0; i<results.length; i++ )
+  // Delete Friend
+  // Incoming: userID(current user logged in), friendID(friend to delete)
+  // Outgoing: error message
+  app.post('/api/deletefriend', async function(req, res, next)
   {
-    _ret.push( results[i].Card );
-  }
-  
-  var ret = {results:_ret, error:error};
-  res.status(200).json(ret);
-  });
-app.post('/api/addcard', async (req, res, next) =>
-  {
-    // incoming: userId, color
-    // outgoing: error
-    const { userId, card } = req.body;
-    const newCard = {Card:card,UserId:userId};
+    // Gets input JSON
+    const { userID, friendID } = req.body;
+
+    // Initializes error
     var error = '';
+
+    // Attempts to find the current user
+    const currentUser = await User.findOne({ userID:userID });
+
+    // If-else statement to catch errors
+    if(currentUser){ // Error if current user doesn't exist
+      if(currentUser.Friends.includes(friendID)){ // Error if user isn't friends with the on trying to be deleted
+        await User.findOneAndUpdate({userID:userID}, {$pull: {Friends:friendID}});
+      }else{
+        error = 'Error. Not friends with user trying to be deleted.';
+      }
+    }else{
+      error = 'Error. Current user does not exist.';
+    }
+
+    // Return with error message
+    var ret = { error };
+    res.status(200).json(ret);
+  });
+
+  // Search for friends to add
+  // Incoming: userID(current user logged in)
+  // Outgoing: users(list of all possible accounts for user to befriend), error message
+  app.post('/api/searchnewfriends', async function(req, res, next)
+  {
+    // Gets input JSON
+    const { userID } = req.body;
+
+    // Initializes error
+    var error = '';
+
+    // Finds the current user
+    const currentUser = await User.findOne( {userID: userID} );
+
+    // Creates an array of the user and their friends
+    const friendsPlusUser = currentUser.Friends;
+    friendsPlusUser.push(userID);
+
+    // Finds all the users except for the current user and their friends
+    const users = await User.find({ userID: { $nin: friendsPlusUser} });
+
+    // Return with error message
+    var ret = {users, error};
+    res.status(200).json(ret);
+  });
+
+  // Search current friends
+  // incoming: userID(current user logged in)
+  // outgoing: users(current user's friends), error message
+  app.post('/api/searchcurrentfriends', async function(req, res, next)
+  {
+    // Gets input JSON
+    const { userID } = req.body;
+
+    // Initializes error
+    var error = '';
+
+    // Finds the current users
+    const currentUser = await User.findOne( {userID: userID} );
+
+    // Finds all of the current user's friends
+    const currentFriends = await User.find( {userID: {$in: currentUser.Friends} } );
+
+    // Return with error message
+    var ret = {currentFriends, error};
+    res.status(200).json(ret);
+  });
+
+  // Add new score
+  // Incoming: userID, score
+  // Outgoing: N/A
+  app.post('/api/addscore', async function(req, res, next)
+  {
+    // Gets input JSON
+    const { userID, score } = req.body;
+
+    // Initializes error
+    var error = '';
+
+    // Finds the current users
+    const currentUser = await User.findOne( {userID: userID} );
+
+    // Gets the current user's username
+    const username = currentUser.Username;
+
+    // Creates a new score for the user
+    const newScore = {userID, Username:username, Score:score};
+
+    // Attempts to add the new score to the table
     try
     {
-      const db = client.db();
-      const result = db.collection('Cards').insertOne(newCard);
+      const result = await Score.create(newScore);
     }
     catch(e)
     {
-      error = e.toString();
+      error = 'Error adding new score';
     }
-    var ret = { error: error };
+
+    // Return with error message
+    var ret = {error};
     res.status(200).json(ret);
   });
-  */
+
+  // Show local leaderboard
+  // Incoming: userID(current user logged in)
+  // Outgoing: username and scores of the current user every user that the current user is friends with
+  app.post('/api/showlocalleaderboard', async function(req, res, next)  
+  {
+    // Gets input JSON
+    const { userID } = req.body;
+
+    // Initializes error
+    var error = '';
+
+    // Finds the current user
+    const currentUser = await User.findOne( {userID: userID} );
+
+    // Creates an array of the user and their friends
+    const friendsPlusUser = currentUser.Friends;
+    friendsPlusUser.push(userID);
+
+    // Finds the scores of the current user and their friends
+    const localScores = await Score.find( {userID: {$in: friendsPlusUser}} ).sort( {Score: -1 } );
+
+    // Return with error message
+    var ret = {localScores, error};
+    res.status(200).json(ret);
+  });
+
+  // Show global leaderboard
+  // Incoming: N/A
+  // Outgoing: username and scores of every user
+  app.post('/api/showgloballeaderboard', async function(req, res, next)
+  {
+    // Initializes error
+    var error = '';
+
+    // Finds every score
+    const globalScores = await Score.find().sort( {Score: -1 } );
+
+    // Return with error message
+    var ret = {globalScores, error};
+    res.status(200).json(ret);
+  });
 }
