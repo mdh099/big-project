f32c83b8004139719c5d4a752d0795a66be8566b
Arthur Upfield <cereusware@icloud.com>
2022-04-14 17:26:12 -0400

    Fixing missing packages. Rollback point 1

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import '../Login.css';
+import axios from 'axios';                        // JWT
 
 function Login()
 {
 
     let bp = require('./Path.js');
+    var storage = require('../tokenStorage.js');  // JWT
 
     var loginName;
     var loginPassword;
@@ -33,36 +35,75 @@ function Login()
         var obj = {login:loginName.value,password:loginPassword.value};
         var js = JSON.stringify(obj);
 
-
-        try
-        {    
-            console.log("here1");
-            const response = await fetch(bp.buildPath('api/login'),
-                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-            var res = JSON.parse(await response.text());
-
-            console.log(res);
-            //---------------------------------------------------------------------------
-            if(res.userID <= 0 || !res.IsVerified)
+         var config =
+        {
+            method: 'post',
+            url: bp.buildPath('api/login'),    // JWT
+            headers:
             {
-                setMessage('Login Failed. Ensure that Your Account is Verified');
+                'Content-Type': 'application/json'
+            },
+            data: js
+        };
+
+        axios(config)
+            .then(function (response)
+        {
+            var res = response.data;
+            if (res.error)
+            {
+                setMessage('User/Password combination incorrect');
             }
             else
-            {                                             // Changed res.id to res.userID
-                var user = {username:res.username,id:res.userID, email:res.email}
-                // added next line for testing
-                console.log(user);
+            {
+                storage.storeToken(res);
+                var jwt = require('jsonwebtoken');
+
+                var ud = jwt.decode(storage.retrieveToken(),{complete:true});
+                var userId = ud.payload.userId;
+                var firstName = ud.payload.firstName;
+                var lastName = ud.payload.lastName;
+
+                var user = {firstName:firstName,lastName:lastName,id:userId}
                 localStorage.setItem('user_data', JSON.stringify(user));
-                setMessage('');
-                window.location.href = '/account';
+                window.location.href = '/cards';
             }
-            //------------------------------------------------------------------------
-        }
-        catch(e)
+        })
+        .catch(function (error)
         {
-            console.log(e.toString());
-            return;
-        }    
+            console.log(error);
+        });
+
+
+//         try
+//         {
+//             console.log("here1");
+//             const response = await fetch(bp.buildPath('api/login'),
+//                 {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+//             var res = JSON.parse(await response.text());
+//
+//             console.log(res);
+//             //---------------------------------------------------------------------------
+//             if(res.userID <= 0 || !res.IsVerified)
+//             {
+//                 setMessage('Login Failed. Ensure that Your Account is Verified');
+//             }
+//             else
+//             {                                             // Changed res.id to res.userID
+//                 var user = {username:res.username,id:res.userID, email:res.email}
+//                 // added next line for testing
+//                 console.log(user);
+//                 localStorage.setItem('user_data', JSON.stringify(user));
+//                 setMessage('');
+//                 window.location.href = '/account';
+//             }
+//             //------------------------------------------------------------------------
+//         }
+//         catch(e)
+//         {
+//             console.log(e.toString());
+//             return;
+//         }
     };
 
     return(
