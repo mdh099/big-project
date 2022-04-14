e0d97109715742d131300f36ab3ea7bad420b365
MrRadir <83057195+MrRadir@users.noreply.github.com>
2022-04-14 18:30:01 -0400

    SomeDatabase stuff done

---
+++ AR-Asteroids/Assets/Scripts/DatabaseAccess.cs
@@ -0,0 +1,65 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DatabaseAccess : MonoBehaviour
+{
+    /*
+    MongoClient client = new MongoClient("mongodb + srv://asteroidRoot:ztyXeYpqyD65IUId@asteroidscluster.q4aw1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    IMongoDatabase database;
+    IMongoCollection<BsonDocument> collection;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        database = client.GetDatabase("Asteroids");
+        collection = database.GetCollection<BsonDocument>("NameOfThing");
+
+        //temp for testing Watch 13:50
+        //GetScoresFromDataBase();
+    }
+
+
+    public async void SaveScoreToDataBase(string userName, int score)
+    {
+        var document = new BsonDocument { { userName, score } };
+        await collection.InsertOneAsync(document);
+    }
+
+    public async Task<List<HighScore>> GetScoresFromDataBase()
+    {
+        var allScoresTask = collection.FindAsync(new BsonDocument());
+        var scoresAwaited = await allScoresTask;
+
+        List<HighScore> highScores = new List<HighScore>();
+        foreach(var score in scoresAwaited.ToList())
+        {
+            highScores.Add(Deserialize(score.ToString()));
+        }
+
+        return highScores;
+    }
+
+    private HighScore Deserialize(string rawJson)
+    {
+        var highScore = new HighScore();
+
+        return highScore;
+    }
+    */
+}
+
+/*
+//inline class
+public class HighScore
+{
+    public string UserName { get; set; }
+    public int Score { get; set; }
+}
+*/
