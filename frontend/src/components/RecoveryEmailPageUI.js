11b415f54fc9c39a38b11b44aaf6fd911cce1b48
Arthur Upfield <cereusware@icloud.com>
2022-04-17 01:42:24 -0400

    Reset almost done, just needs to read from URL bar.

---
+++ frontend/src/components/RecoveryEmailPageUI.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import './PasswordResetPageUI.css';
+
+function RecoveryEmailPageUI()
+{
+    var newPassword;
+    var response2;
+    const [message,setMessage] = useState('');
+
+    var url;
+    var token;
+
+    const app_name = 'cop4331-123'
+
+    function buildPath(route)
+    {
+        if (process.env.NODE_ENV === 'production')
+        {
+            return 'https://' + app_name +  '.herokuapp.com/' + route;
+        }
+        else
+        {
+            return 'http://localhost:5000/' + route;
+        }
+    }
+
+    const gotoLogin = async event =>
+    {
+        event.preventDefault();
+        try
+        {
+            window.location.href = '/login';
+
+        }
+        catch(e)
+        {
+            console.log(e.toString());
+            return;
+        }
+    };
+
+    const passwordReset = async event =>
+    {
+
+
+        token = url;
+        console.log();
+
+//         event.preventDefault();
+//         var obj = {"resetToken":token, password:newPassword.value};
+//
+//         var js = JSON.stringify(obj);
+//         try
+//         {
+//             const response = await fetch(buildPath('api/verifyreset'), {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
+//
+//             var res = JSON.parse(await response.text());
+//
+//             console.log(res.error);
+//             console.log(res.message);
+//
+//             if( res.error !== "" )
+//             {
+//                 if (obj.newPassword == "")
+//                 {
+//                     setMessage('Enter a new password');
+//                 }
+//                 else
+//                 {
+//                     setMessage('Error: ' + res.error);
+//                 }
+//             }
+//
+//             if (res.message !== "")
+//             {
+//                 setMessage('Success!');
+//                 //window.location.href = '/login';
+//             }
+//         }
+//         catch(e)
+//         {
+//             console.log(e.toString());
+//             return;
+//         }
+    };
+
+    return (
+        <div id="changePasswordDiv">
+
+        <form onSubmit={passwordReset}>
+        <br/>
+        <br/>
+        <div id = "accInfo">
+                Please enter a new password:
+        </div>
+        <br/>
+
+        <div class="grid-container">
+            <div class="passG">
+                <input type="password" id="loginPassword" placeholder="New Password" ref={(c) => newPassword = c} />
+            </div>
+            <div class="emailG">
+                <input type="submit" id="loginButton" class="buttons" value = "▷"
+                onClick={passwordReset} />
+            </div>
+        </div>
+
+        </form>
+
+        <br/>
+        <br/>
+        <span id="loginResult">{message}</span>
+        <br/>
+        <br/>
+        <loginLink id="gotoVerifyLink" onClick={gotoLogin} >Remember your password?</loginLink>
+
+        </div>
+    );
+};
+
+export default RecoveryEmailPageUI;
---
+++ frontend/src/pages/RecoveryEmailPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import Register from '../components/Register';
+import RecoveryEmailPageUI from '../components/RecoveryEmailPageUI';
+
+const RecoveryEmailPage = () =>
+{
+    return(
+      <div>
+        <PageTitle />
+        <RecoveryEmailPageUI />
+      </div>
+    );
+};
+export default RecoveryEmailPage;
